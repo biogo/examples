00e9a6985eb75139d21df2f04c71b879cbd04bfb
kortschak <dan.kortschak@adelaide.edu.au>
2013-01-29 20:54:40 +1030

    Example of how to modify core biogo type behaviour
    This example wraps a linear.Seq with a new SetDescription method that
    allows UCSC style decription lines to be parsed into the sequence
    annotation metadata.

---
+++ ucsc/ucsc.go
@@ -0,0 +1,61 @@
+// Copyright ©2013 The bíogo Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// ucsc is an example client of biogo.examples/ucsc/ucsc. I reads a set of
+// UCSC exported FASTA sequences and parses the location information in the
+// sequence description into the sequence metadata, converting the 1-based
+// UCSC position information into 0-based half-open used by bíogo. It then
+// prints out the FASTA, preceded by a summary of the location.
+package main
+
+import (
+	"code.google.com/p/biogo.examples/ucsc/ucsc"
+
+	"code.google.com/p/biogo/alphabet"
+	"code.google.com/p/biogo/feat"
+	"code.google.com/p/biogo/io/seqio/fasta"
+	"code.google.com/p/biogo/seq"
+
+	"fmt"
+	"io"
+	"strings"
+)
+
+var fa = `>hg19_dna range=chr18:78016000-78016181 5'pad=0 3'pad=0 strand=+ repeatMasking=none
+AGAGGGAGGATTATTATAATATTGGAATAAAGAGTAATTGCTATCAACTA
+ATGATTAATGATATTCATATATAATCATGTCTAAGATCTATATCTGGTAT
+AACTATTCTTGTTTTATATTTTATTGGAGTGGAACAGCTCATGTCCTCGG
+TCTCTTGCCTCGGCAAAGATTAGATTAGGGTT
+>hg19_dna range=chr18:78015995-78016181 5'pad=5 3'pad=0 strand=+ repeatMasking=none
+ATTATAGAGGGAGGATTATTATAATATTGGAATAAAGAGTAATTGCTATC
+AACTAATGATTAATGATATTCATATATAATCATGTCTAAGATCTATATCT
+GGTATAACTATTCTTGTTTTATATTTTATTGGAGTGGAACAGCTCATGTC
+CTCGGTCTCTTGCCTCGGCAAAGATTAGATTAGGGTT
+>hg19_dna range=chr18:78016000-78016181 5'pad=0 3'pad=0 strand=- repeatMasking=none
+AACCCTAATCTAATCTTTGCCGAGGCAAGAGACCGAGGACATGAGCTGTT
+CCACTCCAATAAAATATAAAACAAGAATAGTTATACCAGATATAGATCTT
+AGACATGATTATATATGAATATCATTAATCATTAGTTGATAGCAATTACT
+CTTTATTCCAATATTATAATAATCCTCCCTCT
+>hg19_dna range=chr18:78016000-78016186 5'pad=5 3'pad=0 strand=- repeatMasking=none
+CACCTAACCCTAATCTAATCTTTGCCGAGGCAAGAGACCGAGGACATGAG
+CTGTTCCACTCCAATAAAATATAAAACAAGAATAGTTATACCAGATATAG
+ATCTTAGACATGATTATATATGAATATCATTAATCATTAGTTGATAGCAA
+TTACTCTTTATTCCAATATTATAATAATCCTCCCTCT
+`
+
+func main() {
+	buf := strings.NewReader(fa)
+	r := fasta.NewReader(buf, ucsc.NewSeq("", nil, alphabet.DNA))
+	for {
+		s, err := r.Read()
+		if err != nil {
+			if err != io.EOF {
+				panic(err)
+			}
+			break
+		}
+		fmt.Printf("\nChr:%s Start:%d End:%d Len:%d Strand:%v\n\n%60a\n",
+			s.Location(), s.Start(), s.End(), s.Len(), seq.Strand(s.(feat.Orienter).Orientation()), s)
+	}
+}
---
+++ ucsc/ucsc/ucsc.go
@@ -0,0 +1,85 @@
+// Copyright ©2013 The bíogo Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package ucsc provides a linear sequence type that parses UCSC header data into
+// the sequence annotation data.
+package ucsc
+
+import (
+	"code.google.com/p/biogo/alphabet"
+	"code.google.com/p/biogo/feat"
+	"code.google.com/p/biogo/seq"
+	"code.google.com/p/biogo/seq/linear"
+
+	"strconv"
+	"strings"
+)
+
+// A Chr is a feat.Feature marking a chromosome.
+type Chr string
+
+func (c Chr) Start() int             { return 0 }
+func (c Chr) End() int               { return 0 }
+func (c Chr) Len() int               { return 0 }
+func (c Chr) Name() string           { return string(c) }
+func (c Chr) Description() string    { return "chromosome" }
+func (c Chr) Location() feat.Feature { return nil }
+
+// Seq modifies the behaviour of linear.Seq so that the description is parsed
+// according to the UCSC format.
+type Seq struct {
+	*linear.Seq
+}
+
+// NewSeq returns a new Seq.
+func NewSeq(id string, b []byte, alpha alphabet.Alphabet) Seq {
+	return Seq{linear.NewSeq("", nil, alphabet.DNA)}
+}
+
+// Clone returns a copy of the Seq.
+func (s Seq) Clone() seq.Sequence {
+	return Seq{s.Seq.Clone().(*linear.Seq)}
+}
+
+// SetDescription sets the Desc of the embedded linear.Seq and parses
+// the relevant fields of the description to populate the location, offset
+// and strand fields of the sequence annotation.
+func (s Seq) SetDescription(d string) error {
+	const (
+		rangeField  = "range="
+		strandField = "strand="
+	)
+
+	var (
+		start int
+		err   error
+	)
+	for _, f := range strings.Fields(d) {
+		switch {
+		case strings.HasPrefix(f, rangeField):
+			rf := strings.FieldsFunc(f[len(rangeField):], func(r rune) bool { return r == ':' || r == '-' })
+			if len(rf) < 1 {
+				continue
+			}
+			s.Loc = Chr(rf[0])
+			if len(rf) < 2 {
+				continue
+			}
+			start, err = strconv.Atoi(rf[1])
+		case strings.HasPrefix(f, strandField):
+			st := f[len(strandField):]
+			if len(st) == 0 {
+				continue
+			}
+			if st[0] == '+' {
+				s.Strand = seq.Plus
+			} else if st[0] == '-' {
+				s.Strand = seq.Minus
+			}
+		}
+	}
+	s.Offset = feat.OneToZero(start)
+	s.Desc = d
+	return err
+}
