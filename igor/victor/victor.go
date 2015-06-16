a2713af555cf0702fa3d67b25732445648f59a56
kortschak <dan.kortschak@adelaide.edu.au>
2015-06-17 09:12:46 +0930

    victor: fix package doc comment

--- igor/victor/victor.go
+++ igor/victor/victor.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// victore is a post processor for grouping families defined by igor.
+// victor is a post processor for grouping families defined by igor.
 package main
 
 import (
