328ff952ac565ac541bdf0da3e00f8a0bd8cbb2a
kortschak <dan.kortschak@adelaide.edu.au>
2017-11-06 11:50:09 +1030

    igor/{gffer,seqer}: move into own directories

--- igor/seqer/seqer.go
+++ igor/seqer/seqer.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build ignore
-
 // seqer returns multiple fasta sequences corresponding to feature intervals
 // described in the JSON output from igor. It will also produce fastq consensus
 // sequence output from one of MUSCLE or MAFFT.
